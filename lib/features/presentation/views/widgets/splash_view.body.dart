6567d53f253197ef849e48019371db1887bdd5a1
Mohamed Eldemerdash <mohamedeldemerdash6@gmail.com>
2023-01-10 19:39:07 +0200

    splash view done

--- lib/features/presentation/views/widgets/splash_view.body.dart
+++ lib/features/presentation/views/widgets/splash_view.body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -6,6 +8,20 @@ class SplashViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssestData.loge),
+        const SizedBox(
+          height: 4,
+        ),
+        const Text(
+          'Read Free Books',
+          style: TextStyle(fontSize: 14, color: Colors.white),
+          textAlign: TextAlign.center,
+        )
+      ],
+    );
   }
 }
