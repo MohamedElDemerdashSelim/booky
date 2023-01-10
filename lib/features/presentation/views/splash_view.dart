2e2d08886fbf795e8ba31fc5e26f365d436b1a6a
Mohamed Eldemerdash <mohamedeldemerdash6@gmail.com>
2023-01-10 18:56:10 +0200

    create spash view view & splash view body

---
+++ lib/features/presentation/views/splash_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'widgets/splash_view.body.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SplashViewBody();
+  }
+}
