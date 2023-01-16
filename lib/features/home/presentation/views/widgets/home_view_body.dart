81c343e47ecb16fc64540b3d488b9b9a606da377
Mohamed Eldemerdash <mohamedeldemerdash6@gmail.com>
2023-01-16 19:11:22 +0200

    best seller list view item part one

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -25,6 +25,36 @@ class HomeViewBody extends StatelessWidget {
             'Best Seller',
             style: Styles.titleMedium,
           ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.red,
+                image: const DecorationImage(
+                    fit: BoxFit.fill, image: AssetImage(AssetsData.testimage)),
+              ),
+            ),
+          ),
+          Column(
+            children: [],
+          ),
         ],
       ),
     );
