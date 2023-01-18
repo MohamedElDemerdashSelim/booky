03ce3e6d44193eb2f055b4acd530aaf7da375940
Mohamed Eldemerdash <mohamedeldemerdash6@gmail.com>
2023-01-18 18:39:11 +0200

    create custom book details app bar

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:go_router/go_router.dart';
 
@@ -5,6 +6,8 @@ import '../../features/splash/presentation/views/splash_view.dart';
 
 abstract class AppRouter {
   static const kHomeview = '/homeview';
+  static const kBookdetailsview = '/bookdetailsview';
+
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -14,6 +17,10 @@ abstract class AppRouter {
       GoRoute(
         path: kHomeview,
         builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: kBookdetailsview,
+        builder: (context, state) => const BookDetailsView(),
       ),
     ],
   );
--- lib/features/splash/presentation/views/widgets/splash_view.body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view.body.dart
@@ -1,8 +1,9 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:bookly/constants.dart';
+import 'package:go_router/go_router.dart';
 
 import 'sliding_text.dart';
 
@@ -63,8 +64,9 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
   void navigateToHome() {
     Future.delayed(const Duration(seconds: 2), () {
-      Get.to(() => const HomeView(),
-          transition: Transition.fade, duration: kTransitionDuration);
+      GoRouter.of(context).push(AppRouter.kHomeview);
+      // Get.to(() => const HomeView(),
+      //     transition: Transition.fade, duration: kTransitionDuration);
     });
   }
 }
