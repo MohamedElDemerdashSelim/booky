e7a0c9f3b2aceb55d5a7111e2f1218601bc7c54e
Mohamed Eldemerdash <mohamedeldemerdash6@gmail.com>
2023-01-18 18:53:31 +0200

    add book image and  to book details view body

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
@@ -10,9 +11,15 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+
     return Column(
-      children: const [
-        CustomBookDetailsAppBar(),
+      children: [
+        const CustomBookDetailsAppBar(),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * .20),
+          child: const CustomBookImage(),
+        ),
       ],
     );
   }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -8,7 +8,7 @@ import 'best_seller_list_view.dart';
 import 'best_seller_list_view_item.dart';
 import 'custom_app_bar.dart';
 import 'featured_Books_list_view.dart';
-import 'featured_list_view.dart';
+import 'custom_book_image.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
